7e105cd5421c032ad18e12fbfef650fa6e6c937e
randall ishino <rishino24@gmail.com>
2018-08-08 04:35:51 -0700

    shade out the losing team's color

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 window.onload = getMLB;
 
 function getMLB() {
-    const url = 'https://api.mysportsfeeds.com/v2.0/pull/mlb/current/date/20180807/games.json';
+    const url = 'https://api.mysportsfeeds.com/v2.0/pull/mlb/current/date/20180805/games.json';
     // let data = "";
     $.ajax({
         url: url,
@@ -14,37 +14,38 @@ function getMLB() {
 
         success: function (response) {
 
-            for ( let i = 0; i < response.games.length; i++){
+            for (let i = 0; i < response.games.length; i++) {
                 let awayTeam = response.games[i].schedule.awayTeam.abbreviation;
                 let homeTeam = response.games[i].schedule.homeTeam.abbreviation;
                 let status = response.games[i].schedule.playedStatus;
-                console.log(response.games[i]);
                 let homeTeamScore = response.games[i].score.homeScoreTotal;
                 let awayTeamScore = response.games[i].score.awayScoreTotal;
 
-                gameStatus(status,homeTeam,awayTeam,homeTeamScore,awayTeamScore);
-
+                if ( homeTeamScore > awayTeamScore) {
+                    $(".home-team").css("color","grey");
+                }
+                gameStatus(status, homeTeam, awayTeam, homeTeamScore, awayTeamScore);
             }
-            
         },
         error: function (error) {
-            console.log(error);    
+            console.log(error);
         }
     })
 }
 
 
-function gameStatus(status,homeTeam,awayTeam,homeTeamScore,awayTeamScore) {
+function gameStatus(status, homeTeam, awayTeam, homeTeamScore, awayTeamScore) {
     if (homeTeamScore && awayTeamScore != null) {
-    $(".scores").append("<dl class='w-50 w-25-l ph3 mb2'>" +
-    "<dt class='f7 fw8 inning'>"+ status + "</dt>" + 
-    "<dd class='ml0 f5 home-team'>" + homeTeam + " " + homeTeamScore + "</dd>" + 
-    "<dd class='ml0 f5 away-team'>" + awayTeam + " " + awayTeamScore +  "</dd>" + "</dl>");
-}
-else  {
-    $(".scores").append("<dl class='w-50 w-25-l ph3 mb2'>" +
-"<dt class='f7 fw8 inning'>"+ status + "</dt>" + 
-"<dd class='ml0 f5 home-team'>" + homeTeam + " " + "0" + "</dd>" + 
-"<dd class='ml0 f5 away-team'>" + awayTeam + " " + "0" +  "</dd>" + "</dl>");
-}
+        $(".scores").append("<dl class='w-50 w-25-l ph3 mb2'>" +
+            "<dt class='f7 fw8 inning'>" + status + "</dt>" +
+            "<dd class='ml0 f5 home-team'>" + homeTeam + " " + homeTeamScore + "</dd>" +
+            "<dd class='ml0 f5 away-team'>" + awayTeam + " " + awayTeamScore + "</dd>" + "</dl>");
+    }
+    else {
+        $(".scores").append("<dl class='w-50 w-25-l ph3 mb2'>" +
+            "<dt class='f7 fw8 inning'>" + status + "</dt>" +
+            "<dd class='ml0 f5 home-team'>" + homeTeam + " " + "0" + "</dd>" +
+            "<dd class='ml0 f5 away-team'>" + awayTeam + " " + "0" + "</dd>" + "</dl>");
+    }
 };
+
