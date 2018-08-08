ccb0865589979f3c095ea73892a7b238d0b4b2e7
randall ishino <rishino24@gmail.com>
2018-08-07 23:53:53 -0700

    working on .each to retrieve all data and put to scoreboard

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,21 +2,32 @@ window.onload = getMLB;
 
 function getMLB() {
     const url = 'https://api.mysportsfeeds.com/v2.0/pull/mlb/current/date/20180807/games.json';
-
+    let data = "";
     $.ajax({
         url: url,
         method: "GET",
         dataType: "json",
-        async: false,
+        data: data,
         headers: {
             "Authorization": "Basic " + btoa("fc43a60e-d321-498f-9331-9ce7c8" + ":" + "MYSPORTSFEEDS")
         },
 
-        success: function (response) {
-            console.log(response.games[14].schedule);
-            $(".inning").html(response.games[14].schedule.playedStatus);
-            $(".away-team").html(response.games[14].schedule.awayTeam.abbreviation);
-            $(".home-team").html(response.games[14].schedule.homeTeam.abbreviation);
+        success: function (data) {
+            
+            $.each($(data), function() {
+                $.each(this, function(k, v) {
+                  /// do stuff
+                  console.log(k);
+                  console.log(v);
+                  console.log(this.schedule);
+                });
+              });
+            // console.log(response);
+            
+            // console.log(response.games[14].schedule);
+            // $(".inning").html(response.games[14].schedule.playedStatus);
+            // $(".away-team").html(response.games[14].schedule.awayTeam.abbreviation);
+            // $(".home-team").html(response.games[14].schedule.homeTeam.abbreviation);
         },
         error: function (error) {
             console.log(error);    
