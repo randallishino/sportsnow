7ab6a408928421db62bac56d1be07931a3f93080
randall ishino <rishino24@gmail.com>
2018-08-08 02:53:56 -0700

    scoreboard data to page, need to add date selector for games

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,35 +2,38 @@ window.onload = getMLB;
 
 function getMLB() {
     const url = 'https://api.mysportsfeeds.com/v2.0/pull/mlb/current/date/20180807/games.json';
-    let data = "";
+    // let data = "";
     $.ajax({
         url: url,
         method: "GET",
         dataType: "json",
-        data: data,
+        // data: data,
         headers: {
             "Authorization": "Basic " + btoa("fc43a60e-d321-498f-9331-9ce7c8" + ":" + "MYSPORTSFEEDS")
         },
 
-        success: function (data) {
+        success: function (response) {
+
+            for ( let i = 0; i < response.games.length; i++){
+                let awayTeam = response.games[i].schedule.awayTeam.abbreviation;
+                let homeTeam = response.games[i].schedule.homeTeam.abbreviation;
+                let status = response.games[i].schedule.playedStatus;
+                console.log(response.games[i]);
+                let homeTeamScore = response.games[i].score.homeScoreTotal;
+                let awayTeamScore = response.games[i].score.awayScoreTotal;
+
+
+                $(".scores").append("<dl class='w-50 w-25-l ph3 mb2'>" +
+                 "<dt class='f7 fw8 inning'>"+ status + "</dt>" + 
+                 "<dd class='ml0 f5 home-team'>" + homeTeam + " " + homeTeamScore + "</dd>" + 
+                 "<dd class='ml0 f5 away-team'>" + awayTeam + " " + awayTeamScore +  "</dd>" + "</dl>");
+            }
             
-            $.each($(data), function() {
-                $.each(this, function(k, v) {
-                  /// do stuff
-                  console.log(k);
-                  console.log(v);
-                  console.log(this.schedule);
-                });
-              });
-            // console.log(response);
-            
-            // console.log(response.games[14].schedule);
-            // $(".inning").html(response.games[14].schedule.playedStatus);
-            // $(".away-team").html(response.games[14].schedule.awayTeam.abbreviation);
-            // $(".home-team").html(response.games[14].schedule.homeTeam.abbreviation);
         },
         error: function (error) {
             console.log(error);    
         }
     })
 }
+
+
