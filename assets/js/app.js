4f269f3542256b0a3a67b7da7ad0293b1bcefdfe
randall ishino <rishino24@gmail.com>
2018-08-08 19:19:14 -0700

    added colors for final/live games, parsed and added time to display if game was not played

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,11 @@
 window.onload = getMLB;
 
 function getMLB() {
-    const url = 'https://api.mysportsfeeds.com/v2.0/pull/mlb/current/date/20180805/games.json';
-    // let data = "";
+    const url = 'https://api.mysportsfeeds.com/v2.0/pull/mlb/current/date/201808010/games.json';
     $.ajax({
         url: url,
         method: "GET",
         dataType: "json",
-        // data: data,
         headers: {
             "Authorization": "Basic " + btoa("fc43a60e-d321-498f-9331-9ce7c8" + ":" + "MYSPORTSFEEDS")
         },
@@ -15,16 +13,22 @@ function getMLB() {
         success: function (response) {
 
             for (let i = 0; i < response.games.length; i++) {
-                let awayTeam = response.games[i].schedule.awayTeam.abbreviation;
-                let homeTeam = response.games[i].schedule.homeTeam.abbreviation;
+                const awayTeam = response.games[i].schedule.awayTeam.abbreviation;
+                const homeTeam = response.games[i].schedule.homeTeam.abbreviation;
                 let status = response.games[i].schedule.playedStatus;
-                let homeTeamScore = response.games[i].score.homeScoreTotal;
-                let awayTeamScore = response.games[i].score.awayScoreTotal;
-
-                if ( homeTeamScore > awayTeamScore) {
-                    $(".home-team").css("color","grey");
+                const homeTeamScore = response.games[i].score.homeScoreTotal;
+                const awayTeamScore = response.games[i].score.awayScoreTotal;
+                const parsedTime = (moment(response.games[i].schedule.startTime));
+                const scheduledTime = (moment(parsedTime).format("dddd, MMMM Do YYYY, h:mm:ss a"));
+                // if (status === "UNPLAYED") {
+                //     $("dt").removeClass("inning").addClass('not_played');
+                //     $(".not_played").html(scheduledTime);
+                // }    
+                if ( status === "UNPLAYED"){
+                    status = scheduledTime;
                 }
-                gameStatus(status, homeTeam, awayTeam, homeTeamScore, awayTeamScore);
+                scoreStatus(status, homeTeam, awayTeam, homeTeamScore, awayTeamScore);
+                inningStatus(status,scheduledTime);
             }
         },
         error: function (error) {
@@ -34,7 +38,7 @@ function getMLB() {
 }
 
 
-function gameStatus(status, homeTeam, awayTeam, homeTeamScore, awayTeamScore) {
+function scoreStatus(status, homeTeam, awayTeam, homeTeamScore, awayTeamScore) {
     if (homeTeamScore && awayTeamScore != null) {
         $(".scores").append("<dl class='w-50 w-25-l ph3 mb2'>" +
             "<dt class='f7 fw8 inning'>" + status + "</dt>" +
@@ -49,3 +53,12 @@ function gameStatus(status, homeTeam, awayTeam, homeTeamScore, awayTeamScore) {
     }
 };
 
+function inningStatus(status,scheduledTime) {
+    if ( status === "COMPLETED") {
+        $("dt").removeClass("inning").addClass("final");
+        $(".final").html("FINAL");
+    }
+    else {
+        $(".inning").css("color","blue");
+    }
+};
